fbc19bd1b16138263fb36622809a0d09f20053ef
swarpatel23 <smp1613@gmail.com>
2020-04-26 17:44:34 +0530

    final deployment

---
+++ client/deploy.sh
@@ -0,0 +1,16 @@
+docker build -t swar23/multi-client:latest -t swar23/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t swar23/multi-server:latest -t swar23/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t swar23/multi-worker:latest -t swar23/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push swar23/multi-client:$SHA
+docker push swar23/multi-worker:$SHA
+docker push swar23/multi-server:$SHA
+
+docker push swar23/multi-client:latest
+docker push swar23/multi-worker:latest
+docker push swar23/multi-server:latest
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=swar23/multi-server:$SHA
+kubectl set image deployments/client-deployments client=swar23/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=swar23/multi-worker:$SHA
