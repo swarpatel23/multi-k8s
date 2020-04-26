9f22080b22a0a79a249f1fb18085900c29e582df
swarpatel23 <smp1613@gmail.com>
2020-04-26 17:52:29 +0530

    final deployment

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push swar23/multi-worker:latest
 docker push swar23/multi-server:latest
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=swar23/multi-server:$SHA
-kubectl set image deployments/client-deployments client=swar23/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=swar23/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=swar23/multi-server:$SHA
+kubectl set image deployments/client-deployment client=swar23/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=swar23/multi-worker:$SHA
